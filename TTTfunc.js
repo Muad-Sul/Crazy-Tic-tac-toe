687ea2eda132e6450ec17b003b368aff97a12f98
Muad Suleiman <muad.suleiman@gmail.com>
2026-01-09 16:12:53 -0600

    Added hover effect, playable cells, and place logic

--- TTTfunc.js
+++ TTTfunc.js
@@ -1,2 +1,14 @@
 const statusText = document.querySelector(".status");
 statusText.textContent = "X's turn";
+
+
+const cells = document.querySelectorAll(".cell");  //gets all cells
+cells.forEach(cell => {  //goes thru every cell one at a time
+    cell.addEventListener("click", () => { //when this cell is clicked
+        if (cell.textContent !== "") return;  //dont overwrite
+        cell.textContent = "X"; //place X
+        statusText.textContent = "X placed";  //UI feedback
+
+    });
+});
+
