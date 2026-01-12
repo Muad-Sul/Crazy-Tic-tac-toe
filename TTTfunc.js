199e55e2d78bb1b4389503143a91727539b50b37
Muad Suleiman <muad.suleiman@gmail.com>
2026-01-12 16:34:48 -0600

    Added XO phases and click handler

--- TTTfunc.js
+++ TTTfunc.js
@@ -1,14 +1,23 @@
 const statusText = document.querySelector(".status");
+let currentPlayer = "X"; //remembers whos turn it is
 statusText.textContent = "X's turn";
 
-
 const cells = document.querySelectorAll(".cell");  //gets all cells
-cells.forEach(cell => {  //goes thru every cell one at a time
+
+cells.forEach(cell => {  //loop over cells
     cell.addEventListener("click", () => { //when this cell is clicked
-        if (cell.textContent !== "") return;  //dont overwrite
-        cell.textContent = "X"; //place X
-        statusText.textContent = "X placed";  //UI feedback
+        if (cell.textContent !== "") 
+            return;  //dont overwrite
+
+        cell.textContent = currentPlayer; //place X
+
+        if (cell.textContent == 'X'){
+            currentPlayer = "O";
+        }else{
+            currentPlayer = "X";
+        }
+            statusText.textContent = currentPlayer + "'s turn";
+        
 
     });
 });
-
